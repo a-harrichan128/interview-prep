a163d8534a39762bdd918737bf1d5123d097866e
Avinash Harrichan <a_harrichan@live.ca>
2022-08-16 12:53:34 -0400

    Completed exercise 3, with write up

--- Day1/exercise3.py
+++ Day1/exercise3.py
@@ -14,15 +14,28 @@ class Solution(object):
         elif target > nums[right]:
             return len(nums)
 
-        while ((right - left) != 1):
+        while ((right - left) >= 10):
+            print(str(left) + " middle " + str(right))
             pivot = int((left + right) / 2)
             if (nums[pivot] == target):
                 found = pivot
             elif (nums[pivot] < target):
+                left = pivot
+            elif (nums[pivot] > target):
                 right = pivot
 
+        while (left <= right):
+            if (nums[left] == target):
+                found = left
+                left = right + 1
+            elif (nums[left] < target):
+                left = left + 1
+            elif (nums[left] > target):
+                found = left
+                left = right + 1
+        return found
 
 s1 = Solution()
-nums = [1, 3, 5, 7]
-target = 9
+nums = [1,4,6,7,8,9]
+target = 6
 print(s1.searchInsert(nums, target))
